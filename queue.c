8750d8bc68a5d8c3dbfd91711e34ecb46f41f5e4
Alex <mathewalex806@gmail.com>
2023-10-21 14:33:15 +0530

    queue.c

--- queue.c
+++ queue.c
@@ -1,82 +1,95 @@
- #include <stdio.h>
- int front = -1 , rear  = -1, n;
-
-void enqueue(int n, int a[n], int front, int rear)
+#include <stdio.h>
+int front = -1;
+int rear = -1; 
+int n;
+int  loop = 0;
+void enqueue(int a[])
 {
-    if (rear == n-1)
+    if (rear == -1 && front ==-1)
+    {
+        front = front +1;
+        rear = rear +1;
+        printf("Enter element \t");
+        scanf("%d",&a[rear]);
+
+    }
+     else if (rear == n-1)
     {
         printf("Queue overflow");
 
     }
-    else if (rear == -1 && front ==-1)
-    {
-        front ++;
-        rear ++;
-        printf("Enter element");
-        scanf("%d",&a[rear]);
-    }
     else
     {
-        rear ++;
-        printf("Enter element");
+        rear = rear +1;
+        printf("Enter element \t");
         scanf("%d",&a[rear]);
     }
 }
 
 
-void dequeue(int n, int a[n], int front , int rear)
+void dequeue(int a[])
 {
     if (front == -1 && rear == -1)
     {
         printf("Queue underflow");
     }
-    else if (front == n-1 )
-    {}
+    else if( front > rear || front > n-1)
+    {
+        printf("Queue is empty");
+    }
     else
     {
-        printf("Element removed from the queue is  %d",a[front]);
+        printf("Element removed is %d", a[front]);
         front ++;
     }
 }
 
 
+void size()
+{
+
+    int size = rear-front +1;
+    printf("Size of the queue is %d",size);
+
+}
+
+void exit_queue()
+{
+    loop = 1;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
- void main()
- {
-    int choice;
-    printf("Queue Program");
-    printf("Enter size of queue");
+void main()
+{
+    printf("Enter size of the queue \t");
     scanf("%d",&n);
     int a[n];
-    printf("Menu\n 1.Enqueue \n 2.Dequeue \n Enter your choice : ");
-    scanf("%d",&choice);
-    switch (choice)
+    int choice;
+    while (loop == 0)
     {
-    case 1:
-        //enqueue
-        break;
+            printf("\n");
+            printf("Menu \n 1.Enqueue \n 2.Dequeue \n 3.Size \n 4.Exit \n Enter your choice (1-4): \t");
+            scanf("%d",&choice);
+            switch (choice)
+            {
+                case (1):
+                    enqueue(a);
+                    break;
+                
+                case (2):
+                    dequeue(a);
+                    break;
+                
+                case (3):
+                    size();
+                    break;
+                case (4):
+                    exit_queue();
+                    break;
+                default:
+                    printf("Invalid choice ");
+                    break;
 
-
-    case 2:
-        // dequeue
-        break;
-
-
-    default:
-        break;
+            }
     }
-
-    
- }
+}
